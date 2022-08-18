e629efc18ed4e26ab43f3219d0f9f6a7ba6ba69e
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-08-18 10:10:44 -0500

    Add test for app settings

--- AsyncDemo.Web/Controllers/Api/StatusController.cs
+++ AsyncDemo.Web/Controllers/Api/StatusController.cs
@@ -7,13 +7,45 @@ namespace AsyncDemo.Web.Controllers.Api;
 [Route("/status")]
 public class StatusController : BaseApiController
 {
+    private readonly IConfiguration _config;
+
+    /// <summary>
+    /// Status Controller
+    /// </summary>
+    /// <param name="configuration"></param>
+    public StatusController(IConfiguration configuration)
+    {
+        this._config = configuration;
+    }
     /// <summary>
     /// Returns Current Application Status
     /// </summary>
     /// <returns></returns>
     [HttpGet]
+    [Route("/")]
     public ApplicationStatus Get()
     {
         return GetApplicationStatus();
     }
+
+    /// <summary>
+    /// Get App Settings
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("/appsettings")]
+    public object GetAppSettings()
+    {
+        string[] TestIdList = _config.GetStringList("Async:TestIds");
+        string[] MissingList = _config.GetStringList("Async:Bad");
+        string[] MissingDefault = _config.GetStringList("Async:Bad", "3,2,1");
+        string[] MissingDefaultSingle = _config.GetStringList("Async:Bad", "99");
+        int[] TestIdIntList = _config.GetIntList("Async:TestIds");
+        int[] MissingIntList = _config.GetIntList("Async:Bad");
+        int[] MissingDefaultInt = _config.GetIntList("Async:Bad", "3,2,1");
+        int[] BadDefaultInt = _config.GetIntList("Async:Bad", "bob,sam,1");
+        return new { TestIdList, MissingList, MissingDefault, MissingDefaultSingle, TestIdIntList, MissingIntList, MissingDefaultInt, BadDefaultInt };
+    }
 }
+
+
